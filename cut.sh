9652f6fa2556ac57d3c5e4bca4fc65c5341a8ee9
Andrew Rea <email@andrewrea.co.uk>
2018-10-24 15:20:13 +0000

    Extra checks

--- cut.sh
+++ cut.sh
@@ -6,14 +6,22 @@ echo "NEXT VERSION: $NEXT_VERSION"
 cd "$DIR" 
 
 DEVELOP_VERSION="$(git describe --abbrev=0 remotes/origin/develop)"
-MASTER_VERSION="$(git describe --abbrev=0 remotes/origin/develop)"
+MASTER_VERSION="$(git describe --abbrev=0 remotes/origin/master)"
+
+echo "Develop: $DEVELOP_VERSION Master: $MASTER_VERSION"
 
 if [[ "$DEVELOP_VERSION" == "$MASTER_VERSION"  ]]; then 
-  git checkout master 
-  git pull --no-edit origin develop
-  git tag -a "$NEXT_VERSION" -m "$NEXT_VERSION" || :
-  git push origin master 
-  git push origin "$NEXT_VERSION" || :
+  if [[ "$(git describe --abbrev=0 remotes/origin/develop)" == "$(git describe remotes/origin/develop)" ]]; then
+    echo "No change - nothing to do"
+  else
+    git checkout master 
+    git pull origin master
+    git pull --no-edit origin develop
+    git push origin master 
+    
+    git tag -a "$NEXT_VERSION" -m "$NEXT_VERSION" || :
+    git push origin "$NEXT_VERSION" || :
+  fi
 else
   echo "Version mis-match. Develop: $DEVELOP_VERSION Master: $MASTER_VERSION"
 fi
