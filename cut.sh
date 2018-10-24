c5ed5dc1be30e9b7c677e9a4cce2b35e67c55ff0
Andrew Rea <email@andrewrea.co.uk>
2018-10-24 13:23:43 +0000

    Failing silently

--- cut.sh
+++ cut.sh
@@ -11,9 +11,9 @@ MASTER_VERSION="$(git describe --abbrev=0 remotes/origin/develop)"
 if [[ "$DEVELOP_VERSION" == "$MASTER_VERSION"  ]]; then 
   git checkout master 
   git pull --no-edit origin develop
-  git tag -a "$NEXT_VERSION" -m "$NEXT_VERSION"
-  git push origin master
-  git push origin "$NEXT_VERSION"
+  git tag -a "$NEXT_VERSION" -m "$NEXT_VERSION" || :
+  git push origin master 
+  git push origin "$NEXT_VERSION" || :
 else
   echo "Version mis-match. Develop: $DEVELOP_VERSION Master: $MASTER_VERSION"
 fi
