13f82f48bb382f91d7d5815cce396af3cf3d2910
Andrew Rea <email@andrewrea.co.uk>
2018-10-24 13:12:36 +0000

    Updating cut with extra protection

--- cut.sh
+++ cut.sh
@@ -4,7 +4,15 @@ DIR=$1
 NEXT_VERSION=$(./semverit $(realpath "$DIR"))
 echo "NEXT VERSION: $NEXT_VERSION"
 cd "$DIR" 
-git checkout master 
-git pull origin develop
-git tag -a "$NEXT_VERSION" -m "$NEXT_VERSION"
-git push origin "$NEXT_VERSION"
+
+DEVELOP_VERSION="$(git describe --abbrev=0 origin/develop)"
+MASTER_VERSION="$(git describe --abbrev=0 origin/develop)"
+
+if [[ "$DEVELOP_VERSION" == "$MASTER_VERSION"  ]]; then 
+  git checkout master 
+  git pull --no-edit origin develop
+  git tag -a "$NEXT_VERSION" -m "$NEXT_VERSION"
+  git push origin "$NEXT_VERSION"
+else
+  echo "Version mis-match. Develop: $DEVELOP_VERSION Master: $MASTER_VERSION"
+fi
