caa22c100d74517dcb9a37907f544029bf35fdf1
Andrew Rea <email@andrewrea.co.uk>
2018-10-24 11:58:12 +0000

    Re-ordering the actions

--- cut.sh
+++ cut.sh
@@ -4,9 +4,7 @@ DIR=$1
 NEXT_VERSION=$(./semverit $(realpath "$DIR"))
 echo "NEXT VERSION: $NEXT_VERSION"
 cd "$DIR" 
-git checkout develop 
+git checkout master 
+git pull origin develop
 git tag -a "$NEXT_VERSION" -m "$NEXT_VERSION"
 git push origin "$NEXT_VERSION"
-git checkout master
-git merge "$NEXT_VERSION"
-git push origin master
